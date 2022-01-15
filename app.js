2a80f7edb152a498c08318a2b28720b355e46b6a
Matías Lokman <matias.lokman@gmail.com>
2022-01-15 17:13:02 -0300

    App.js created

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = process.env.APP_PORT;
 
-const port = 3000;
+// const postRouter = require("./routes/post-routes");
+const userRouter = require("./routes/user-routes");
+
+const errorAppMiddleware = require("./middleware/errorAppMiddleware");
+const { notFoundMiddleware } = require("./middleware/notFoundMiddleware");
+
+const {
+  RegistrationDev,
+  RegistrationArchive,
+} = require("./middleware/registrationMiddleware");
+
+const { appTimestampMiddleware } = require("./middleware/appMiddleware");
+
+const sequelize = require("./database/integradorConnection");
+
+app.use([RegistrationDev, RegistrationArchive]);
+
+app.use(appTimestampMiddleware);
+
+app.use(userRouter);
+
+app.all("*", notFoundMiddleware);
+
+app.use(errorAppMiddleware);
 
 app.listen(port, "localhost", () => {
-  console.log(`app running on port ${port}`);
+  console.log("app corriendo en el puerto " + port);
 });
