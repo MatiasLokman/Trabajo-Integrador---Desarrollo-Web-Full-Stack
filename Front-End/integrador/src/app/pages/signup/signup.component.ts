eaecea9de17766e40d5b2d8ad2c4ef118bda5d7e
Santimv <santimv01@gmail.com>
2022-01-18 20:52:38 -0300

    feat: signup form

--- Front-End/integrador/src/app/pages/signup/signup.component.ts
+++ Front-End/integrador/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormBuilder,  FormControl,  FormGroup,  RequiredValidator,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  public countries = ["Argentina", "Brazil", "Chile"]
+  public cities = ["Cordoba", "Bs As", "Tucuman"]
+  public newUserData = FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signup(data: []){
+    console.log(data)
   }
 
 }
