52e5fb88e7791f672c82b21cc0ebda7a4cbe2ea3
Santimv <santimv01@gmail.com>
2022-01-17 23:51:00 -0300

    feat: login styles

--- Front-End/integrador/src/app/material/material.module.ts
+++ Front-End/integrador/src/app/material/material.module.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-let materialModules = [MatToolbarModule, MatIconModule, MatButtonModule];
+
+let materialModules = [
+  MatToolbarModule, 
+  MatIconModule, 
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [],
