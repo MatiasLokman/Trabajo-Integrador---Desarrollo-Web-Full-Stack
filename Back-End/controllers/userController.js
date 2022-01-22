71fa2c907937bfa7702a3b2183d38dcfcb6d52a5
Matías Lokman <matias.lokman@gmail.com>
2022-01-22 11:23:54 -0300

    feat: user controllers

--- Back-End/controllers/userController.js
+++ Back-End/controllers/userController.js
@@ -15,19 +15,38 @@ const getUserAll = async (req, res) => {
 // -----------------------------------------------------------------------------------------------------------------------//
 
 const receivedMessagesById = async (req, res) => {
-  //  ...
+    const { id } = req.params;
+  
+    const result = await Message.findOne({
+      where: {
+        id: id,
+      },
+      include: User,
+    });
+    res.json({ result });
 };
 
 // -----------------------------------------------------------------------------------------------------------------------//
 
 const sentMessagesById = async (req, res) => {
-  //  ...
+    const { id } = req.params;
+  
+    const result = await Message.findOne({
+      where: {
+        id: id,
+      },
+      include: User,
+    });
+    res.json({ result });
 };
 
 // -----------------------------------------------------------------------------------------------------------------------//
 
 const SendMessageToId = async (req, res) => {
-  //  ...
+    const data = ({ message, id_user, id_receiver } = req.body);
+  
+    const message = await Message.create(data);
+    res.json({ status: http.StatusCodes.OK, data: message });
 };
 
 // -----------------------------------------------------------------------------------------------------------------------//
