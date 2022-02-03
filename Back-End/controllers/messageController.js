00675d888950a790f3d3fce2dc08059d71c1a2e8
Matías Lokman <matias.lokman@gmail.com>
2022-02-02 21:16:43 -0300

    feat: message controller updated

--- Back-End/controllers/messageController.js
+++ Back-End/controllers/messageController.js
@@ -3,12 +3,13 @@ const http = require("http-status-codes");
 
 // -----------------------------------------------------------------------------------------------------------------------//
 
-const deleteMessage = (req, res) => {
-const { id_message } = req.params;
+const deleteMessage = async (req, res) => {
+const { id: id_message } = req.params;
 const message = await Message.findOne({where: {id_message}})
 await message.destroy()
 // verificar que el mensaje pertenece al usuario
 // verificar que el mensaje existe
+res.json({ status: http.StatusCodes.OK, data: "Message deleted successfully"});
 };
 
 // -----------------------------------------------------------------------------------------------------------------------//
