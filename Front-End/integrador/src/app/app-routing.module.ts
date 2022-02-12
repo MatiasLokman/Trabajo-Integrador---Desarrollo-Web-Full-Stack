90354b42a322d1e28c06669752168c95e90ac696
Patricio Pagano <pagano.patricio@gmail.com>
2022-02-12 12:34:36 -0300

    feat: guards created

--- Front-End/integrador/src/app/app-routing.module.ts
+++ Front-End/integrador/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatedGuard } from './guards/authenticated.guard';
+import { UnauthenticatedGuard } from './guards/unauthenticated.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { InboxComponent } from './pages/messages/inbox/inbox.component';
 import { NewMessageComponent } from './pages/messages/new-message/new-message.component';
@@ -7,15 +9,31 @@ import { SentComponent } from './pages/messages/sent/sent.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  
-  { path: 'signup', component: SignupComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [UnauthenticatedGuard],
+  },
 
-  { path: 'new-message', component: NewMessageComponent },
-  
-  { path: 'inbox', component: InboxComponent },
-  
-  { path: 'sent', component: SentComponent },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    canActivate: [UnauthenticatedGuard],
+  },
+
+  {
+    path: 'new-message',
+    component: NewMessageComponent,
+    canActivate: [AuthenticatedGuard],
+  },
+
+  {
+    path: 'inbox',
+    component: InboxComponent,
+    canActivate: [AuthenticatedGuard],
+  },
+
+  { path: 'sent', component: SentComponent, canActivate: [AuthenticatedGuard] },
 
   {
     path: '',
